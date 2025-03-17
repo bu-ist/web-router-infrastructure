38a2c797d8237db274662ecee386582450010625
Henry Louis-Charles <hl-c@bu.edu>
2025-03-16 22:51:32 -0400

    Version 1 of the WAF update

--- waf-ansible/adjust-waf.py
+++ waf-ansible/adjust-waf.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(description="Adjust WAF rule actions based on n
 parser.add_argument("--WebACLName", help="Name of WebACL")
 parser.add_argument("--WebACLID", help="ID of the WebACL")
 parser.add_argument("--Scope", choices=["REGIONAL","CLOUDFRONT"], help="Scope of the WebACL")
+parser.add_argument("--Region", choices=["us-east-1","us-east-2"], help="Region of the WebACL")
 parser.add_argument("--WhitelistRule", choices=["Allow", "Block", "Count"], help="Action for Whitelist rules")
 parser.add_argument("--BlacklistRule", choices=["Allow", "Block", "Count"], help="Action for Blacklist rules")
 parser.add_argument("--ReputationListsRule", choices=["Allow", "Block", "Count"], help="Action for ReputationLists rules")
@@ -16,11 +17,11 @@ parser.add_argument("--BadBotRule", choices=["Allow", "Block", "Count"], help="A
 parser.add_argument("--SqlInjectionRule", choices=["Allow", "Block", "Count"], help="Action for SqlInjection rules")
 parser.add_argument("--XssRule", choices=["Allow", "Block", "Count"], help="Action for Xss rules")
 parser.add_argument("--ScannersAndProbesRule", choices=["Allow", "Block", "Count"], help="Action for ScannersAndProbes rules")
-parser.add_argument("--HttpFloodRateBasedRule", choices=["Allow", "Block", "Count"], help="Action for HttpFloodRateBased rules")
+parser.add_argument("--HttpFloodRateBasedRule", choices=["Block", "Count"], help="Action for HttpFloodRateBased rules")
 args = parser.parse_args()
 
 # Initialize boto3 client
-client = boto3.client('wafv2', region_name='us-east-1')
+client = boto3.client('wafv2', region_name=args.Region)
 
 # Collect webACL as a json
 data = client.get_web_acl(
@@ -35,7 +36,7 @@ def adjust_rule_action(rules, match, action):
         if match in rule["Name"]:
             rule["Action"] = {action: {}}
 
-# Example usage
+# Change the Rules
 adjust_rule_action(data["WebACL"]["Rules"], "WhitelistRule", args.WhitelistRule)
 adjust_rule_action(data["WebACL"]["Rules"], "BlacklistRule", args.BlacklistRule)
 adjust_rule_action(data["WebACL"]["Rules"], "ReputationListsRule", args.ReputationListsRule)
@@ -46,7 +47,7 @@ adjust_rule_action(data["WebACL"]["Rules"], "ScannersAndProbesRule", args.Scanne
 adjust_rule_action(data["WebACL"]["Rules"], "HttpFloodRateBasedRule", args.HttpFloodRateBasedRule)
 
 # Output result as JSON
-print(json.dumps(data, indent=4))
+#print(json.dumps(data, indent=4))
 
 # WebACL update payload
 response = client.update_web_acl(
